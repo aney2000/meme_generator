6d0dc04ae17d3ee8d027534e2bd2cf1cf6b9e5de
Dragan Andrei <aney2000@yahoo.com>
2026-07-11 11:30:47 +0300

    Refactor classes

--- services/db.rb
+++ services/db.rb
@@ -9,20 +9,22 @@ Dir.mkdir(DB_DIR) unless Dir.exist?(DB_DIR)
 DB_PATH = File.join(DB_DIR, 'users.db')
 DB = Sequel.sqlite(DB_PATH)
 
-unless DB.table_exists?(:users)
+if DB.table_exists?(:users)
+  column_names = DB.schema(:users).map { |column| column[0] }
+
+  unless column_names.include?(:token)
+    DB.add_column :users, :token, String
+
+    DB[:users].where(token: nil).each do |user|
+      DB[:users].where(id: user[:id]).update(token: SecureRandom.hex(16))
+    end
+  end
+else
   DB.create_table :users do
     primary_key :id
     String :username, unique: true, null: false
     String :password_hash, null: false
     String :token, unique: true, null: false
     DateTime :created_at, default: Sequel::CURRENT_TIMESTAMP
-  end
-else
-  unless DB.columns(:users).include?(:token)
-    DB.add_column :users, :token, String
-
-    DB[:users].where(token: nil).each do |user|
-      DB[:users].where(id: user[:id]).update(token: SecureRandom.hex(16))
-    end
   end
 end
